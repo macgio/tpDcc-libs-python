b30d6d663368f58f6697c2224976412072a45548
Tomi Poveda <tpovedatd@gmail.com>
2019-09-17 00:41:04 +0200

    Added dict merge function to merge dict recursively

--- source/tpPyUtils/python.py
+++ source/tpPyUtils/python.py
@@ -15,6 +15,7 @@ import ast
 import uuid
 import types
 import traceback
+import collections
 
 from six import text_type
 
@@ -705,3 +706,23 @@ def u_print(msg, **kwargs):
             pass
         msg = msg.encode(encoding or 'utf-8', 'replace')
     print(msg, **kwargs)
+
+
+def dict_merge(dict, merge_dict):
+    """
+    Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+
+    :param dct: dict onto which the merge is executed
+    :param merge_dct: dct merged into dct
+    :return: None
+    """
+
+    for k, v in merge_dct.iteritems():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
