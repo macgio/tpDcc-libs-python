1538934b5c4b9cc7b3975790d9c3781abfd258cb
Tomi Poveda <tpovedatd@gmail.com>
2020-02-04 20:07:59 +0100

    Added decorator to dynamicall add a function to a specific class

--- tpPyUtils/decorators.py
+++ tpPyUtils/decorators.py
@@ -309,6 +309,23 @@ def cached(fn):
     return wrapper
 
 
+def add_method(cls):
+    """
+    This decorator adds a function to given class instance
+    https://medium.com/@mgarod/dynamically-add-a-method-to-a-class-in-python-c49204b85bd6
+    :param cls: class instance
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            return func(self, *args, **kwargs)
+        setattr(cls, func.__name__, wrapper)
+        # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
+        return func # returning func means func can still be used normally
+    return decorator
+
+
 class Singleton(object):
     all_instances = list()
 
